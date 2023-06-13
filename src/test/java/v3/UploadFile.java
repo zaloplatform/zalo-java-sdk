133df1b6942e1bde2f04f74e225ac88f7d871178
hienhh <hienhh@vng.com.vn>
2023-06-13 09:26:16 +0700

    update readme

--- src/test/java/v3/UploadFile.java
+++ src/test/java/v3/UploadFile.java
@@ -21,13 +21,13 @@ public class UploadFile {
 
     public static void main(String[] args) throws APIException {
         ZaloOaClient client = new ZaloOaClient();
-        String access_token = "your_access_token";
+        String access_token = "DlZc09wi6pCm_kGEdwiQFpBxwGAGjGzbKe610-MLBJGvXhDTvDGoFpJcjs_FxYm55lcKRTkjDp1u-RLZkEiA86VVzswgqGqGQUA0HPhF37OmcwOj_OiRIWMAw3hqW5DtNRJV4VACNaGRdziwnQSa23sik3Jejbu0EQF3DVcnRGSKa9LQri0O2dhJaLotzGugGEQTMEhP9cyHtwaYkFSGV7FoY4c4vWSuUkEARfApSGbFdDjXWPfw4NcGXo3EjI1f0kNR1AtoTKDH_lC0WCTIQapIxHoEynLt9yFf7zME555tiUyQtB1TMnAKtc_0atiR5yVASRp-E1OVxRvRlCOP6LGpcc3B19Ys6JK";
 
         Map<String, String> headers = new HashMap<>();
         headers.put("access_token", access_token);
 
         Map<String, File> files = new HashMap<>();
-        File file = new File("/home/lap13561/Pictures/Screenshot from 2022-08-09 15-04-06.png");
+        File file = new File("/home/lap13561/Downloads/A21402.pdf");
         files.put("file", file);
 
         JsonObject excuteRequest = client.excuteRequest("https://dev-openapi.zalo.me/v2.0/oa/upload/file", "POST", null, null, headers, files);
